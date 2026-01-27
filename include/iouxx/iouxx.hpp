2b069afb2d842c05716738d4c847fef467108f8f
xiaosa@work <dr.xiaosa@gmail.com>
2026-01-27 14:54:56 +0800

    Update universal header

--- include/iouxx/iouxx.hpp
+++ include/iouxx/iouxx.hpp
@@ -13,8 +13,10 @@
 #include "iouops/noop.hpp" // IWYU pragma: export
 #include "iouops/timeout.hpp" // IWYU pragma: export
 #include "iouops/cancel.hpp" // IWYU pragma: export
+#include "iouops/futex.hpp" // IWYU pragma: export
 #include "iouops/network/socketio.hpp" // IWYU pragma: export
 #include "iouops/file/fileio.hpp" // IWYU pragma: export
+#include "iouops/file/poll.hpp" // IWYU pragma: export
 
 namespace iouxx::details {
 
