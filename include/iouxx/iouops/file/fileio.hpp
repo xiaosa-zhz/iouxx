ae27e707dde604395a24d75d086881e61bcb9955
xiaosaOvO <dr.xiaosa@gmail.com>
2026-02-22 00:43:34 +0800

    Add readv writev operations

--- include/iouxx/iouops/file/fileio.hpp
+++ include/iouxx/iouops/file/fileio.hpp
@@ -16,6 +16,31 @@
 
 #endif // IOUXX_USE_CXX_MODULE
 
+IOUXX_EXPORT
+namespace iouxx::inline iouops::fileops {
+
+    enum class rw_flag {
+        none    = 0,
+        hipri   = RWF_HIPRI,
+        dsync   = RWF_DSYNC,
+        sync    = RWF_SYNC,
+        nowait  = RWF_NOWAIT,
+        append  = RWF_APPEND,
+    };
+
+    constexpr rw_flag operator|(rw_flag lhs, rw_flag rhs) noexcept {
+        return static_cast<rw_flag>(
+            std::to_underlying(lhs) | std::to_underlying(rhs)
+        );
+    }
+
+    constexpr rw_flag& operator|=(rw_flag& lhs, rw_flag rhs) noexcept {
+        lhs = lhs | rhs;
+        return lhs;
+    }
+
+} // namespace iouxx::iouops::fileops
+
 namespace iouxx::details {
 
     class file_read_buffer_operation_base
@@ -77,6 +102,19 @@ namespace iouxx::details {
         bool is_fixed = false;
         int fd = -1;
         std::size_t off = 0;
+    };
+
+    class rw_flag_base
+    {
+    public:
+        template<typename Self>
+        Self& options(this Self& self, fileops::rw_flag flags) noexcept {
+            self.flags = flags;
+            return self;
+        }
+
+    protected:
+        fileops::rw_flag flags = fileops::rw_flag::none;
     };
 
 } // namespace iouxx::details
@@ -161,16 +199,10 @@ namespace iouxx::inline iouops::fileops {
 
         static constexpr std::uint8_t opcode = IORING_OP_READ_FIXED;
 
-        template<utility::buffer_like Buffer>
-        file_read_fixed_operation& buffer(Buffer&& buf, int index) & noexcept {
-            this->details::file_read_buffer_operation_base::buffer(std::forward<Buffer>(buf));
+        file_read_fixed_operation& index(int index) & noexcept {
             this->buf_index = index;
             return *this;
         }
-
-        // Shadow the file_read_write_operation_base::buffer method to avoid misuse
-        template<utility::buffer_like Buffer>
-        file_read_fixed_operation& buffer(Buffer&&) & noexcept = delete;
 
     private:
         friend operation_base;
@@ -190,7 +222,7 @@ namespace iouxx::inline iouops::fileops {
             }
         }
 
-        int buf_index = -1;
+        int buf_index = alloc_index;
         [[no_unique_address]] callback_type callback;
     };
 
@@ -200,6 +232,151 @@ namespace iouxx::inline iouops::fileops {
     template<typename F, typename... Args>
     file_read_fixed_operation(iouxx::ring&, std::in_place_type_t<F>, Args&&...)
         -> file_read_fixed_operation<F>;
+
+    template<std::size_t MaxIOvecs>
+    class file_readv
+    {
+        static constexpr std::size_t max_iovecs = MaxIOvecs;
+    public:
+        template<utility::eligible_callback<std::ptrdiff_t> Callback>
+        class operation final : public operation_base,
+            public details::file_read_write_operation_base,
+            public details::rw_flag_base
+        {
+        public:
+            template<utility::not_tag F>
+            explicit operation(iouxx::ring& ring, F&& f)
+                noexcept(utility::nothrow_constructible_callback<F>) :
+                operation_base(iouxx::op_tag<operation>, ring),
+                callback(std::forward<F>(f))
+            {}
+
+            template<typename F, typename... Args>
+            explicit operation(iouxx::ring& ring, std::in_place_type_t<F>, Args&&... args)
+                noexcept(std::is_nothrow_constructible_v<F, Args...>) :
+                operation_base(iouxx::op_tag<operation>, ring),
+                callback(std::forward<Args>(args)...)
+            {}
+
+            using callback_type = Callback;
+            using result_type = std::ptrdiff_t;
+
+            static constexpr std::uint8_t opcode = IORING_OP_READV;
+
+            template<utility::buffer_like Buffer>
+            operation& buffer(std::size_t off, Buffer&& buf) & noexcept {
+                auto span = utility::to_buffer(std::forward<Buffer>(buf));
+                iovecs[off].iov_base = span.data();
+                iovecs[off].iov_len = span.size();
+                return *this;
+            }
+
+        private:
+            friend operation_base;
+            void build(::io_uring_sqe* sqe) & noexcept {
+                ::io_uring_prep_readv2(sqe, fd,
+                    iovecs.data(), iovecs.size(),
+                    off, std::to_underlying(flags));
+                if (is_fixed) {
+                    sqe->flags |= IOSQE_FIXED_FILE;
+                }
+            }
+
+            void do_callback(int ev, std::uint32_t) IOUXX_CALLBACK_NOEXCEPT_IF(
+                utility::eligible_nothrow_callback<callback_type, result_type>) {
+                if (ev >= 0) {
+                    std::invoke_r<void>(callback, static_cast<std::ptrdiff_t>(ev));
+                } else {
+                    std::invoke_r<void>(callback, utility::fail(-ev));
+                }
+            }
+
+            std::array<::iovec, max_iovecs> iovecs = {};
+            [[no_unique_address]] callback_type callback;
+        };
+
+        template<utility::eligible_callback<std::ptrdiff_t> F>
+        operation(iouxx::ring&, F) -> operation<std::decay_t<F>>;
+
+        template<typename F, typename... Args>
+        operation(iouxx::ring&, std::in_place_type_t<F>, Args&&...) -> operation<F>;
+    };
+
+    template<std::size_t MaxIOvecs>
+    class file_readv_fixed
+    {
+        static constexpr std::size_t max_iovecs = MaxIOvecs;
+    public:
+        template<utility::eligible_callback<std::ptrdiff_t> Callback>
+        class operation final : public operation_base,
+            public details::file_read_write_operation_base,
+            public details::rw_flag_base
+        {
+        public:
+            template<utility::not_tag F>
+            explicit operation(iouxx::ring& ring, F&& f)
+                noexcept(utility::nothrow_constructible_callback<F>) :
+                operation_base(iouxx::op_tag<operation>, ring),
+                callback(std::forward<F>(f))
+            {}
+
+            template<typename F, typename... Args>
+            explicit operation(iouxx::ring& ring, std::in_place_type_t<F>, Args&&... args)
+                noexcept(std::is_nothrow_constructible_v<F, Args...>) :
+                operation_base(iouxx::op_tag<operation>, ring),
+                callback(std::forward<Args>(args)...)
+            {}
+
+            using callback_type = Callback;
+            using result_type = std::ptrdiff_t;
+
+            static constexpr std::uint8_t opcode = IORING_OP_READV_FIXED;
+
+            template<utility::buffer_like Buffer>
+            operation& buffer(std::size_t off, Buffer&& buf) & noexcept {
+                auto span = utility::to_buffer(std::forward<Buffer>(buf));
+                iovecs[off].iov_base = span.data();
+                iovecs[off].iov_len = span.size();
+                return *this;
+            }
+
+            operation& index(int index) & noexcept {
+                this->buf_index = index;
+                return *this;
+            }
+
+        private:
+            friend operation_base;
+            void build(::io_uring_sqe* sqe) & noexcept {
+                ::io_uring_prep_readv_fixed(sqe, fd,
+                    iovecs.data(), iovecs.size(),
+                    off, std::to_underlying(flags),
+                    buf_index);
+                if (is_fixed) {
+                    sqe->flags |= IOSQE_FIXED_FILE;
+                }
+            }
+
+            void do_callback(int ev, std::uint32_t) IOUXX_CALLBACK_NOEXCEPT_IF(
+                utility::eligible_nothrow_callback<callback_type, result_type>) {
+                if (ev >= 0) {
+                    std::invoke_r<void>(callback, static_cast<std::ptrdiff_t>(ev));
+                } else {
+                    std::invoke_r<void>(callback, utility::fail(-ev));
+                }
+            }
+
+            int buf_index = alloc_index;
+            std::array<::iovec, max_iovecs> iovecs = {};
+            [[no_unique_address]] callback_type callback;
+        };
+
+        template<utility::eligible_callback<std::ptrdiff_t> F>
+        operation(iouxx::ring&, F) -> operation<std::decay_t<F>>;
+
+        template<typename F, typename... Args>
+        operation(iouxx::ring&, std::in_place_type_t<F>, Args&&...) -> operation<F>;
+    };
 
     template<utility::eligible_callback<std::ptrdiff_t> Callback>
     class file_write_operation final : public operation_base,
@@ -278,16 +455,10 @@ namespace iouxx::inline iouops::fileops {
 
         static constexpr std::uint8_t opcode = IORING_OP_WRITE_FIXED;
 
-        template<utility::readonly_buffer_like Buffer>
-        file_write_fixed_operation& buffer(Buffer&& buf, int index) & noexcept {
-            this->details::file_write_buffer_operation_base::buffer(std::forward<Buffer>(buf));
+        file_write_fixed_operation& index(int index) & noexcept {
             this->buf_index = index;
             return *this;
         }
-
-        // Shadow the file_read_write_operation_base::buffer method to avoid misuse
-        template<utility::buffer_like Buffer>
-        file_write_fixed_operation& buffer(Buffer&&) & noexcept = delete;
 
     private:
         friend operation_base;
@@ -307,7 +478,7 @@ namespace iouxx::inline iouops::fileops {
             }
         }
 
-        int buf_index = -1;
+        int buf_index = alloc_index;
         [[no_unique_address]] callback_type callback;
     };
 
@@ -317,6 +488,155 @@ namespace iouxx::inline iouops::fileops {
     template<typename F, typename... Args>
     file_write_fixed_operation(iouxx::ring&, std::in_place_type_t<F>, Args&&...)
         -> file_write_fixed_operation<F>;
+
+    template<std::size_t MaxIOvecs>
+    class file_writev
+    {
+        static constexpr std::size_t max_iovecs = MaxIOvecs;
+    public:
+        template<utility::eligible_callback<std::ptrdiff_t> Callback>
+        class operation final : public operation_base,
+            public details::file_read_write_operation_base,
+            public details::rw_flag_base
+        {
+        public:
+            template<utility::not_tag F>
+            explicit operation(iouxx::ring& ring, F&& f)
+                noexcept(utility::nothrow_constructible_callback<F>) :
+                operation_base(iouxx::op_tag<operation>, ring),
+                callback(std::forward<F>(f))
+            {}
+
+            template<typename F, typename... Args>
+            explicit operation(iouxx::ring& ring, std::in_place_type_t<F>, Args&&... args)
+                noexcept(std::is_nothrow_constructible_v<F, Args...>) :
+                operation_base(iouxx::op_tag<operation>, ring),
+                callback(std::forward<Args>(args)...)
+            {}
+
+            using callback_type = Callback;
+            using result_type = std::ptrdiff_t;
+
+            static constexpr std::uint8_t opcode = IORING_OP_WRITEV;
+
+            template<utility::readonly_buffer_like Buffer>
+            operation& buffer(std::size_t off, Buffer&& buf) & noexcept {
+                auto span = utility::to_readonly_buffer(std::forward<Buffer>(buf));
+                iovecs[off].iov_base = const_cast<void*>(
+                    static_cast<const void*>(span.data())
+                );
+                iovecs[off].iov_len = span.size();
+                return *this;
+            }
+
+        private:
+            friend operation_base;
+            void build(::io_uring_sqe* sqe) & noexcept {
+                ::io_uring_prep_writev2(sqe, fd,
+                    iovecs.data(), iovecs.size(),
+                    off, std::to_underlying(flags));
+                if (is_fixed) {
+                    sqe->flags |= IOSQE_FIXED_FILE;
+                }
+            }
+
+            void do_callback(int ev, std::uint32_t) IOUXX_CALLBACK_NOEXCEPT_IF(
+                utility::eligible_nothrow_callback<callback_type, result_type>) {
+                if (ev >= 0) {
+                    std::invoke_r<void>(callback, static_cast<std::ptrdiff_t>(ev));
+                } else {
+                    std::invoke_r<void>(callback, utility::fail(-ev));
+                }
+            }
+
+            std::array<::iovec, max_iovecs> iovecs = {};
+            [[no_unique_address]] callback_type callback;
+        };
+
+        template<utility::eligible_callback<std::ptrdiff_t> F>
+        operation(iouxx::ring&, F) -> operation<std::decay_t<F>>;
+
+        template<typename F, typename... Args>
+        operation(iouxx::ring&, std::in_place_type_t<F>, Args&&...) -> operation<F>;
+    };
+
+    template<std::size_t MaxIOvecs>
+    class file_writev_fixed
+    {
+        static constexpr std::size_t max_iovecs = MaxIOvecs;
+    public:
+        template<utility::eligible_callback<std::ptrdiff_t> Callback>
+        class operation final : public operation_base,
+            public details::file_read_write_operation_base,
+            public details::rw_flag_base
+        {
+        public:
+            template<utility::not_tag F>
+            explicit operation(iouxx::ring& ring, F&& f)
+                noexcept(utility::nothrow_constructible_callback<F>) :
+                operation_base(iouxx::op_tag<operation>, ring),
+                callback(std::forward<F>(f))
+            {}
+
+            template<typename F, typename... Args>
+            explicit operation(iouxx::ring& ring, std::in_place_type_t<F>, Args&&... args)
+                noexcept(std::is_nothrow_constructible_v<F, Args...>) :
+                operation_base(iouxx::op_tag<operation>, ring),
+                callback(std::forward<Args>(args)...)
+            {}
+
+            using callback_type = Callback;
+            using result_type = std::ptrdiff_t;
+
+            static constexpr std::uint8_t opcode = IORING_OP_WRITEV_FIXED;
+
+            template<utility::readonly_buffer_like Buffer>
+            operation& buffer(std::size_t off, Buffer&& buf) & noexcept {
+                auto span = utility::to_readonly_buffer(std::forward<Buffer>(buf));
+                iovecs[off].iov_base = const_cast<void*>(
+                    static_cast<const void*>(span.data())
+                );
+                iovecs[off].iov_len = span.size();
+                return *this;
+            }
+
+            operation& index(int index) & noexcept {
+                this->buf_index = index;
+                return *this;
+            }
+
+        private:
+            friend operation_base;
+            void build(::io_uring_sqe* sqe) & noexcept {
+                ::io_uring_prep_writev_fixed(sqe, fd,
+                    iovecs.data(), iovecs.size(),
+                    off, std::to_underlying(flags),
+                    buf_index);
+                if (is_fixed) {
+                    sqe->flags |= IOSQE_FIXED_FILE;
+                }
+            }
+
+            void do_callback(int ev, std::uint32_t) IOUXX_CALLBACK_NOEXCEPT_IF(
+                utility::eligible_nothrow_callback<callback_type, result_type>) {
+                if (ev >= 0) {
+                    std::invoke_r<void>(callback, static_cast<std::ptrdiff_t>(ev));
+                } else {
+                    std::invoke_r<void>(callback, utility::fail(-ev));
+                }
+            }
+
+            int buf_index = alloc_index;
+            std::array<::iovec, max_iovecs> iovecs = {};
+            [[no_unique_address]] callback_type callback;
+        };
+
+        template<utility::eligible_callback<std::ptrdiff_t> F>
+        operation(iouxx::ring&, F) -> operation<std::decay_t<F>>;
+
+        template<typename F, typename... Args>
+        operation(iouxx::ring&, std::in_place_type_t<F>, Args&&...) -> operation<F>;
+    };
 
 } // namespace iouxx::iouops::fileops
 
